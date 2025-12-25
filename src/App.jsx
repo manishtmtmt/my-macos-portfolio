c8efc2ec82aaf2363dfa74a6b7e9da8547c450e3
manishtmtmt <manisht.mt.mt@gmail.com>
2025-12-25 17:07:51 +0530

    Add Image component; update App and index exports to include Image window

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Dock, Navbar, Welcome } from "@components";
-import { Finder, Resume, Safari, Terminal, Text } from "@windows";
+import { Finder, Image, Resume, Safari, Terminal, Text } from "@windows";
 import gsap from "gsap";
 
 import { Draggable } from "gsap/Draggable";
@@ -18,6 +18,7 @@ const App = () => {
       <Finder />
 
       <Text />
+      <Image />
     </main>
   );
 };
