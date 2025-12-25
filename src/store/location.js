af470bc9e5248059d976b629ec91abd3c44a4e63
manishtmtmt <manisht.mt.mt@gmail.com>
2025-12-25 16:15:41 +0530

    Add Zustand store for location management; implement active location state and reset functionality

---
+++ src/store/location.js
@@ -0,0 +1,21 @@
+const { locations } = require("@constants");
+const { create } = require("zustand");
+const { immer } = require("zustand/middleware/immer");
+
+const DEFAULT_LOCATION = locations.work;
+
+const useLocationStore = create(
+  immer((set) => ({
+    activeLocation: DEFAULT_LOCATION,
+
+    setActiveLocation: (location = null) =>
+      set((state) => {
+        state.activeLocation = location || DEFAULT_LOCATION;
+      }),
+
+    resetActiveLocation: () =>
+      set((state) => (state.activeLocation = DEFAULT_LOCATION)),
+  }))
+);
+
+export default useLocationStore;
