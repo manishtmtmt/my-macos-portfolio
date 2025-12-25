648d6acb81177f3c233f3aef2c5a659cdd5f2162
manishtmtmt <manisht.mt.mt@gmail.com>
2025-12-25 15:25:06 +0530

    Add WindowControls and WindowWrapper components; implement Terminal window with draggable functionality

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar.jsx";
 import Welcome from "./Welcome.jsx";
 import Dock from "./Dock.jsx";
+import WindowControls from "./WindowControls.jsx";
 
-export { Navbar, Welcome, Dock };
+export { Navbar, Welcome, Dock, WindowControls };
